0ab3b9e9074bceb94e8f72a717afee8159eb77a6
Riti Bhatwal <ritibhatwal@gmail.com>
2026-07-05 23:52:00 -0700

    kauydgjuywdfuyQTWRI7tqweriTYQWER

--- Distress Express/Assets/motiontransformer.cs
+++ Distress Express/Assets/motiontransformer.cs
@@ -10,12 +10,19 @@ public enum TileType
 }
 public class motiontransformer : MonoBehaviour
 {
+    public int DebugBreakFinalEntryCount = 0;
+    int DebugBreakCurrentEntryCount = 0;
     public TileType ThisTileType;
-    public GameObject LinearEndPoint;
+    public GameObject LinearEndPointA;
+    public GameObject LinearEndPointB;
+    Collider ColliderPointA;
+    Collider ColliderPointB;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ColliderPointA = LinearEndPointA.GetComponent<Collider>();
+        ColliderPointB = LinearEndPointB.GetComponent<Collider>();
+
     }
 
     // Update is called once per frame
@@ -26,11 +33,20 @@ public class motiontransformer : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        if (DebugBreakFinalEntryCount > 0)
+        {
+            if (DebugBreakCurrentEntryCount == DebugBreakFinalEntryCount)
+            {
+                Debug.Log("debug motion transformer reached");
+            }
+            DebugBreakCurrentEntryCount ++;
+        }
+
         trainscript FoundTrain = other.gameObject.GetComponent<trainscript>();
         switch (ThisTileType)
         {
             case TileType.Linear:
-                Vector3 NewDirection = LinearEndPoint.transform.position - transform.position;
+                Vector3 NewDirection = LinearEndPointB.transform.position - LinearEndPointA.transform.position;
                 FoundTrain.SetDirectionalConstraint(NewDirection);
 
                 break;
