e032fbfb3d0542c2182a3ac9c5c3c47bbca1b5e3
Riti Bhatwal <ritibhatwal@gmail.com>
2026-07-05 23:20:38 -0700

    more stuff its important i promise

--- Distress Express/Assets/trainscript.cs
+++ Distress Express/Assets/trainscript.cs
@@ -5,28 +5,40 @@ using UnityEngine;
 public class trainscript : MonoBehaviour
 {
 
-    public float Speed;
     public Vector3 InitialAcceleration;
+    public Vector3 InitialGravityAcceleration;
+    public bool StartWithDirectionalConstraintOn = true;
 
+    Vector3 CurrentGravityAcceleration;
     Vector3 CurrentAcceleration;
     Vector3 CurrentVelocity;
 
     Vector3 CurrentDirectionalConstraintUnitVec;
-    bool DirectionalConstaintOn = true;
+    bool DirectionalConstaintOn = false;
 
+    float AccelerationTimer = 0;
+    float CurrentAccelerationDuration = 0;
     // Start is called before the first frame update
     void Start()
     {
         CurrentAcceleration = InitialAcceleration;
+        CurrentGravityAcceleration = InitialGravityAcceleration;
+        CurrentAccelerationDuration = 0.5f;
         CurrentDirectionalConstraintUnitVec = Vector3.Normalize(InitialAcceleration);
         CurrentVelocity = new Vector3(0, 0, 0);
-        DirectionalConstaintOn = true;
+        DirectionalConstaintOn = StartWithDirectionalConstraintOn;
     }
 
     // Update is called once per frame
     void Update()
     {
-        CurrentVelocity += CurrentAcceleration * (Time.deltaTime);
+        Vector3 NetAccleration = CurrentGravityAcceleration;
+        if (AccelerationTimer < CurrentAccelerationDuration)
+        {
+            NetAccleration += CurrentAcceleration;
+            AccelerationTimer += Time.deltaTime;
+        }
+        CurrentVelocity += NetAccleration * (Time.deltaTime);
 
         if (DirectionalConstaintOn)
         {
@@ -48,9 +60,10 @@ public class trainscript : MonoBehaviour
         DirectionalConstaintOn = false;
     }
 
-    public void SetAcceleration(Vector3 val)
+    public void SetAcceleration(Vector3 val, float accel_duration)
     {
         CurrentAcceleration = val;
+        CurrentAccelerationDuration = accel_duration;
     }
 
 }
